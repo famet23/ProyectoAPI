e8eab1ce2e57bb2907ea8965debb0bc08b2b1177
Sergio Hurtado <sergiohurtado1209@gmail.com>
2023-09-01 21:11:55 -0300

    aaa

---
+++ src/test/java/com/API/ProyectoAPI/ProyectoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.ProyectoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
